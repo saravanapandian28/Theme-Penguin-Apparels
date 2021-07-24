fe4c371f2996da5780bf4be5ca5d2fb963b92dc1
root <saravanaoff2000@gmail.com>
2021-07-24 18:09:51 +0530

    [ADD] header

---
+++ static/src/js/header.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.menu-btn').click(function(){
+        $('.tonavbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+    })
+
+// header active
+
+var selector = '.tonavbar ul li';
+    var url = window.location.href;
+    var target = url.split('/');
+     $(selector).each(function(){
+        if($(this).find('a').attr('href')===('/'+target[target.length-1])){
+          $(selector).removeClass('activepage');
+          $(this).removeClass('activepage').addClass('activepage');
+        }
+     });
+
+
+
