8936d7509d168d8b26ec91fe464eed55502ba780
root <saravanaoff2000@gmail.com>
2021-08-06 02:09:47 +0530

    [FIX] banner

--- static/src/js/banner.js
+++ static/src/js/banner.js
@@ -1,43 +1,44 @@
+// image change while clicking button
 
-  function displayNextImage() {
+function displayNextImage() {
     x = (x === images.length - 1) ? 0 : x + 1;
     document.getElementById("BannerImg").src = images[x];
 }
-
-document.getElementById("ChangeBannerImage").onclick = function startTimer() {
-    document.getElementById("PlayPause").classList.remove('fa-play');
-    $('#ChangeBannerImage i').toggleClass("fa-pause");
-    var ChangeImageRepeat = setInterval(displayNextImage, 5000);
-    document.getElementById("ChangeBannerImage").onclick = function stopTimer(){
-        clearInterval(ChangeImageRepeat);
-        document.getElementById("PlayPause").classList.remove('fa-pause');
-        $('#ChangeBannerImage i').toggleClass("fa-play");
-        document.getElementById("ChangeBannerImage").onclick = function startTimer(){
-            //repeat
-            document.getElementById("PlayPause").classList.remove('fa-play');
-        $('#ChangeBannerImage i').toggleClass("fa-pause");
-        var ChangeImageRepeat = setInterval(displayNextImage, 5000);
-        document.getElementById("ChangeBannerImage").onclick = function stopTimer(){
-        clearInterval(ChangeImageRepeat);
-        document.getElementById("PlayPause").classList.remove('fa-pause');
-        $('#ChangeBannerImage i').toggleClass("fa-play");
-        document.getElementById("ChangeBannerImage").onclick = function startTimer(){
-            
-        }
-    }
-        }
-    }
-}
-
 var images = [], x = -1;
 images[0] = "/theme_n/static/src/images/aboutus-page-banner.png";
 images[1] = "/theme_n/static/src/images/thebadge-page-banner-web.png";
 images[2] = "/theme_n/static/src/images/Banner-Web-View.png";
 
+var play=document.getElementById("PlayAndPause");
+var count=0;
+var ChangeImageRepeat;
+play.addEventListener('click',function(event){
+    event.preventDefault();
+   
+    if(count==0){
+    
+    play.innerHTML='<i class="fa fa-pause"></i>';
+    play.classList.add("spinner");
+    setTimeout(function(){
+        play.classList.remove("spinner");
+    }, 1000);
+    ChangeImageRepeat = setInterval(displayNextImage, 5000);
+    count++;
+    }
+  else{
+    clearInterval(ChangeImageRepeat);
+      play.innerHTML='<i class="fa fa-play"></i>';
+      play.classList.add("spinner");
+    setTimeout(function(){
+        play.classList.remove("spinner");
+    }, 1000);
+      count--;
+  }  
+})
 
 
-
-  if((window.location.href===('http://localhost:8069/ourproduct'))||(window.location.href===('http://localhost:8069/contactus'))||(window.location.href===('http://localhost:8069/web/login'))||(window.location.href===('http://localhost:8069/process'))){
+//easvh banner for each page
+if((window.location.href===('http://localhost:8069/ourproduct'))||(window.location.href===('http://localhost:8069/contactus'))||(window.location.href===('http://localhost:8069/web/login'))||(window.location.href===('http://localhost:8069/process'))){
     document.getElementById("top-banner").style.display = "none";
   } 
   if(window.location.href===('http://localhost:8069/aboutus')){
@@ -56,3 +57,4 @@ images[2] = "/theme_n/static/src/images/Banner-Web-View.png";
     document.getElementById("BannerTextHome").style.display = "none";
     document.getElementById("ChangeBannerImage").style.display = "none";
   }
+
