461345776034158ee5fa19698c55c07cb67868df
root <saravanaoff2000@gmail.com>
2021-08-06 13:34:21 +0530

    [FIX] banner js

--- static/src/js/banner.js
+++ static/src/js/banner.js
@@ -5,6 +5,7 @@ function displayNextImage() {
     document.getElementById("BannerImg").src = images[x];
 }
 var images = [], x = -1;
+//list of images that need to be change in home page while clicking playbutton
 images[0] = "/theme_n/static/src/images/aboutus-page-banner.png";
 images[1] = "/theme_n/static/src/images/thebadge-page-banner-web.png";
 images[2] = "/theme_n/static/src/images/Banner-Web-View.png";
@@ -20,6 +21,7 @@ play.addEventListener('click',function(event){
     play.innerHTML='<i class="fa fa-pause"></i>';
     play.classList.add("spinner");
     setTimeout(function(){
+      //add spinning class to spin after clicking play button in banner
         play.classList.remove("spinner");
     }, 1000);
     ChangeImageRepeat = setInterval(displayNextImage, 5000);
@@ -37,11 +39,16 @@ play.addEventListener('click',function(event){
 })
 
 
-//easvh banner for each page
-if((window.location.href===('http://localhost:8069/ourproduct'))||(window.location.href===('http://localhost:8069/contactus'))||(window.location.href===('http://localhost:8069/web/login'))||(window.location.href===('http://localhost:8069/process'))){
+
+var url = window.location.href;
+var target = url.split('/');
+//hiding banner for those pages which no need to display
+if((('/'+target[target.length-1])===('/ourproduct'))||(('/'+target[target.length-1])===('/contactus'))||(('/'+target[target.length-1])===('/login'))||(('/'+target[target.length-1])===('/process'))){
     document.getElementById("top-banner").style.display = "none";
   } 
-  if(window.location.href===('http://localhost:8069/aboutus')){
+
+//changing ecvh banner for each page
+  if(('/'+target[target.length-1])===('/aboutus')){
     document.getElementById("BannerImg").src = "/theme_n/static/src/images/aboutus-page-banner.png";
     document.getElementById("BannerTextTwo").innerHTML = '"Whatever your apparel dreams are, we have the expertise to make it a reality"'
     document.getElementById("BannerTextThree").innerHTML = "27 Years in Bussiness, Our passion remains to develop interesting and innovative manufacturing methods to meet customer exceptations";
@@ -49,7 +56,7 @@ if((window.location.href===('http://localhost:8069/ourproduct'))||(window.locati
     document.getElementById("BannerTextHome").style.display = "none";
     document.getElementById("ChangeBannerImage").style.display = "none";
 }
-  if(window.location.href===('http://localhost:8069/thebadge')){
+  if(('/'+target[target.length-1])===('/thebadge')){
     document.getElementById("BannerImg").src = "/theme_n/static/src/images/thebadge-page-banner-web.png";
     document.getElementById("BannerTextTwo").innerHTML = '"Team Work makes our dream work"'
     document.getElementById("BannerTextThree").innerHTML = "Our primary motto is to nature and empower our team";
